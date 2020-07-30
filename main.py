6a52e60bdd7fcb85473eb2570496afe5ce60e3a2
Kevin <kvng88@gmail.com>
2020-07-30 10:28:13 -0700

    updated python script

--- main.py
+++ main.py
@@ -1,29 +1,45 @@
 from bert_serving.client import BertClient
+from flask import Flask
+from flask import request
+from flask import jsonify
+
+import json
 import numpy as np
-import csv
 import torch
-bc = BertClient(ip='**.**.**.**')  # ip address of the GPU machine
 
-with open('file-to-read') as readFile:
-    line_count = 0
-    answers = [i.strip() for i in readFile.readlines()]
-# encode corpus as array of strings
-doc_vecs = bc.encode(answers)  # if tokenized: is_tokenized=True
+app = Flask(__name__)
+if __name__ == "__main__":
+  app.run(debug=True)
 
-while True:
-    query = input('Find matching answer: ')
-    query_vec = bc.encode([query])[0]
-    # convert to torch input
-    tensor_query_vec = torch.from_numpy(query_vec)
-    tensor_doc_vecs = torch.from_numpy(doc_vecs)
-    # compute normalized dot product as score
-    tensor_input = tensor_query_vec * tensor_doc_vecs
-    score = torch.sum(tensor_input, 1) / \
-        torch.norm(tensor_doc_vecs, dim=1)
-    argsort = torch.argsort(score)
-    topk_idx = torch.topk(argsort, 1)
-    nptopk = np.argsort(score)[::-1][:5]
-    print(f'nptopk: {nptopk}')
-    print(f'topk_idx: {topk_idx}')
-    for idx in topk_idx:
-        print('> %s\t%s' % (score[idx], answers[idx]))
+
+@app.route('/analyze/responses', methods=['POST'])
+def analyzeResponses():
+    if request.is_json:
+      data_dict = request.get_json()
+      text = data_dict["text"]
+      # ip address of the GPU machine
+      bc = BertClient(ip='localhost')
+      with open('./server/data/answer-corpus.csv') as readFile:
+          line_count = 0
+          answers = [i.strip() for i in readFile.readlines()]
+      # encode corpus as array of strings
+      doc_vecs = bc.encode(answers)  # if tokenized: is_tokenized=True
+
+      while True:
+          # query = input('Find matching answer: ')
+          query_vec = bc.encode([text])[0]
+          # convert to torch input
+          tensor_query_vec = torch.from_numpy(query_vec)
+          tensor_doc_vecs = torch.from_numpy(doc_vecs)
+          # compute normalized dot product as score
+          tensor_input = tensor_query_vec * tensor_doc_vecs
+          score = torch.sum(tensor_input, 1) / \
+              torch.norm(tensor_doc_vecs, dim=1)
+          argsort = torch.argsort(score)
+          topk_idx = torch.topk(argsort, 1)
+          scores = []
+          for idx in topk_idx:
+              print('> %s\t%s' % (score[idx], answers[idx]))
+              scores.append(answers[idx])
+          print(f'Scores: {scores}')
+          return jsonify(scores[-1]), 201
--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-bert-serving-start -http_port 8125 -num_worker=$1 -model_dir /model/cased_L-24_H-1024_A-16
+bert-serving-start -http_port 8125 -num_worker=$1 -max_seq_len=NONE -model_dir /model/cased_L-24_H-1024_A-16
